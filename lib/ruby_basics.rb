e8e3cc7db1c4bb680b5c1b2bc84eccd2013b0a2a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-16 21:49:20 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,25 +1,25 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
   
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
-
+  puts "Nice"
 end
 
 def last_evaluated_value
-
+  puts "expert"
 end
 
 def pizza_party
